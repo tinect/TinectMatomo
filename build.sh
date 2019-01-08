c1a0079b5093dc0fac716c45bbe05f8db0b4ffb1
tinect <s.koenig@tinect.de>
2019-01-08 21:09:31 +0100

    ....

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 commit=$1
-pluginname=$PLUGIN_NAME
 if [ -z ${commit} ]; then
     commit=$(git tag --sort=-creatordate | head -1)
     if [ -z ${commit} ]; then
@@ -10,11 +9,11 @@ if [ -z ${commit} ]; then
 fi
 
 # Remove old release
-rm -rf ${pluginname} ${pluginname}-*.zip
+rm -rf ${PLUGIN_NAME} ${PLUGIN_NAME}-*.zip
 
 # Build new release
-mkdir -p ${pluginname}
-git archive ${commit} | tar -x -C ${pluginname}
-composer install --no-dev -n -o -d ${pluginname}
-( find ./${pluginname} -type d -name ".git" && find ./${pluginname} -name ".gitignore" && find ./${pluginname} -name ".gitmodules" ) | xargs rm -r
-zip -r ${pluginname}-${commit}.zip ${pluginname}
+mkdir -p ${PLUGIN_NAME}
+git archive ${commit} | tar -x -C ${PLUGIN_NAME}
+composer install --no-dev -n -o -d ${PLUGIN_NAME}
+( find ./${PLUGIN_NAME} -type d -name ".git" && find ./${PLUGIN_NAME} -name ".gitignore" && find ./${PLUGIN_NAME} -name ".gitmodules" ) | xargs rm -r
+zip -r ${PLUGIN_NAME}-${commit}.zip ${PLUGIN_NAME}
